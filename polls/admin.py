c599c2a8df6dbd8d6fdc70c6290344132db080a3
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-09-29 23:16:27 -0400

    Customizing the admin forms

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-
 from .models import Choice, Question
+
+from django.contrib import admin
 
 
 class ChoiceInline(admin.TabularInline):
@@ -16,6 +16,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ("Date information", {"fields": ["pub_date"]}),
     ]
     inlines = [ChoiceInline]
-
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
 
 admin.site.register(Question, QuestionAdmin)
